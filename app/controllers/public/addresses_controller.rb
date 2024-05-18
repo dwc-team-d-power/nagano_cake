ff7e5c502abfdcb084364bd56e5a19a75355f1b2
Area6080 <area6080@gmail.com>
2024-05-18 08:20:28 +0000

    [Create]address_index

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,18 +1,49 @@
 class Public::AddressesController < ApplicationController
-  before_action :authenticate_customer!
-  
+  # before_action :authenticate_customer! 開発時オフにしてます
+  # before_action :is_matching_login_user, only: [:edit, :update, :destroy]
   def index
+    @address = Address.new
+    @addresses = Address.all
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
 
   def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_user.id
+    @address.save
+    redirect_to addresses_path
   end
 
   def update
+    @address = Address.find(params[:id])
+    @address.update(update_address_params)
+    redirect_to addresses_path
   end
 
   def destroy
+    address = Address.find(params[:id])
+    address.destroy
+    redirect_to addresses_path
+  end
+  
+    private
+
+  def address_params
+    params.require(:address).permit(:name, :postal_code, :address, :customer_id)
+  end
+  
+  def update_address_params
+    params.require(:address).permit(:name, :postal_code, :address)
+  end
+  
+  def is_matching_login_user
+    address = Address.find(params[:id])
+    customer = Customer.find(address.customer_id)
+    unless customer.id == current_user.id
+      redirect_to root_path
+    end
   end
 end

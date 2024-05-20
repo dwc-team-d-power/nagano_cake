f51204e434818f7226970c2f1f1d3af445fb4312
Area6080 <area6080@gmail.com>
2024-05-20 07:43:38 +0000

    [Create]order_new

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,6 +2,8 @@ class Customer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :cart_item
+  has_many :address
+  has_many :order
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable

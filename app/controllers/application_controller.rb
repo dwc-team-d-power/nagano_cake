b7039ec84a6789bde1d39b0fa4f421aa4104e7ee
Area6080 <area6080@gmail.com>
2024-05-20 02:36:39 +0000

    [Create]header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,2 @@
 class ApplicationController < ActionController::Base
-      
-  def admin_user?
-    admin.email == current_user.email && admin.encrypted_password == current_user.encrypted_password
-  end
-
 end

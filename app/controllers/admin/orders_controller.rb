df98b94cec726aeb0f940857d987c6deb4ca713f
KentaToya <163114083+kenta1129@users.noreply.github.com>
2024-05-21 08:20:04 +0000

    オーダーまで

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,9 +1,35 @@
-class Admin::OrdersController < ApplicationController
+class Admins::OrdersController < ApplicationController
   before_action :authenticate_admin!
-  
-  def show
+
+  def index
+    @orders = Order.all
   end
 
-  def update
+  def show
+    @order = Order.find(params[:id])
+    @customer = @order.customer
+    @order = @order.order_products
+    @order_product = OrderProduct.find(@admins_order.id)
   end
+
+
+  def update
+    @admins_order = Order.find(params[:id])
+       if @admins_order.update(order_params)
+      redirect_to admins_orders_path, notice: 'ユーザ情報を更新しました。'
+   else
+      render :show
+    @user = @admins_order.user
+    @order_products = @admins_order.order_products
+    @order_product = OrderProduct.find(@admins_order.id)
+   end
+
+  end
+
+private
+
+  def order_params
+    params.require(:order).permit(:user_id, :total_charge, :purchase_date, :payment_method, :order_status, :postage, :destination_address, :destination_name, :destination_postal_code)
+  end
+
 end

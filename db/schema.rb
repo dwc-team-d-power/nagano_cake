9f2529898c80216f265a896ce59ff0941291cf4f
EC2 Default User <161905643+HayatoTakaha@users.noreply.github.com>
2024-05-22 14:40:54 +0000

    コンフリクト解消

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_05_17_012808) do
+ActiveRecord::Schema.define(version: 2024_05_20_142911) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -100,6 +100,7 @@ ActiveRecord::Schema.define(version: 2024_05_17_012808) do
     t.integer "genre_id", null: false
     t.string "name", null: false
     t.text "introduction", null: false
+    t.string "image_id"
     t.integer "price", null: false
     t.boolean "is_active", default: true, null: false
     t.datetime "created_at", precision: 6, null: false

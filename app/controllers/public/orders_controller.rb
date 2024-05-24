897b6f82a756af41e55a5c1078c0ee28ecaa08f0
EC2 Default User <161905643+HayatoTakaha@users.noreply.github.com>
2024-05-24 00:45:56 +0000

    develop_takahaya

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -12,20 +12,22 @@ class Public::OrdersController < ApplicationController
     @order.total_payment = calculate_total_payment
 
     # 住所選択による処理
-    if params[:order][:select_address] == "0"
+    case params[:order][:shipping_address]
+    when '1'
       @order.postal_code = current_customer.postal_code
       @order.address = current_customer.address
-      @order.name = current_customer.name
-    when 'select'
+      @order.name = current_customer.full_name
+    when '2'
       selected_address = Address.find(params[:order][:address_id])
       @order.postal_code = selected_address.postal_code
       @order.address = selected_address.address
       @order.name = selected_address.name
-    when 'new'
+    when '3'
       # すでにフォームから送信されたデータが@orderにセットされているので何もしない
     end
     render :confirm
   end
+
   def create
     @order = Order.new(order_params)
     @order.customer_id = current_customer.id
@@ -46,80 +48,22 @@ class Public::OrdersController < ApplicationController
       render :new
     end
   end
+
   def index
     @orders = current_customer.orders.includes(order_details: :item)
-  end    
+  end
+
   def show
     @order = Order.find(params[:id])
   end
-    
+
   private
 
   def order_params
-    params.require(:order).permit(:payment_method, :address_option,:postal_code, :address, :name)
+    params.require(:order).permit(:payment_method, :shipping_address, :address_id, :postal_code, :address, :name)
   end
-  
+
   def calculate_total_payment
     current_customer.cart_items.sum(&:subtotal) + 800 # 固定の送料を追加
   end
 end
-
-
-
-#   def confirm
-#     @order = Order.new(order_params)
-#     @order.shipping_cost = 800
-    
-#     if params[:order][:shipping_address] == "1"
-#       @order.name = "#{current_customer.last_name} #{current_customer.first_name}"
-#       @order.address = current_customer.address
-#       @order.postal_code = current_customer.postal_code
-      
-#     elsif params[:order][:shipping_address] == "2"
-#       @order.name = Address.find(params[:order][:address_id]).name
-#       @order.address = Address.find(params[:order][:address_id]).address
-#       @order.postal_code = Address.find(params[:order][:address_id]).postal_code
-       
-#     elsif params[:order][:shipping_address] == "3"
-#       address_new = current_customer.addresses.new(address_params)
-#       address_new.save
-      
-#     else
-#       render :new
-#     end
-    
-#     @order.customer_id = current_customer.id
-#     @cart_items = current_customer.cart_items.all
-#     @total = @cart_items.inject(0) { |sum, item| sum + item.subtotal }
-#     # inject(0)初期値は0、{|初期値, 要素| ブロック処理 }itemはcart_itemに変更?
-#     # <% @total +=  cart_item.subtotal %>
-#     @total_payment = @order.shipping_cost + @total
-#   end
-    
-#   def create
-#     @order = current_customer.orders.new(order_params)
-#     @order.save
-#     @cart_items = current_customer.cart_items.all
-    
-#     @cart_items.each do |cart_item|
-#       @order_details = OrderDetail.new
-#       @order_details.order_id = order.id
-#       @order_details.item_id = cart_item.item.id
-#       @order_details.price = cart_item.item.with_tax_price
-#       @order_details.amount = cart_item.amount
-#       @order_details.save
-#     end
-#     current_customer.cart_items.destroy_all
-#     redirect_to orders_thanks_path
-#   end
-
-#   private
-
-#   def order_params
-#     params.require(:order).permit(:name, :address, :postal_code, :payment_method)
-#   end
-  
-#   def address_params
-#     params.require(:address).permit(:name, :address, :postal_code)
-#   end
-# >>>>>>> customer_sakamoto

0dd45ba4cadb129540c863ce08bacbac9afb869a
EC2 Default User <161905643+HayatoTakaha@users.noreply.github.com>
2024-05-27 06:36:56 +0000

    Fix/orders/show

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -17,7 +17,12 @@ class Admin::SessionsController < Devise::SessionsController
     super
   end
 
-  # protected
+  protected
+
+  # 管理者ログイン後のリダイレクト先を設定
+  def after_sign_in_path_for(resource)
+    admin_root_path
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params

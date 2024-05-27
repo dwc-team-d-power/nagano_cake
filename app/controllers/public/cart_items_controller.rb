171e5db87c1886d6b276d0c62d3af309e679422a
Area6080 <area6080@gmail.com>
2024-05-27 04:51:22 +0000

    [Pull]

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -4,30 +4,31 @@ class Public::CartItemsController < ApplicationController
   def index
     @cart_items = current_customer.cart_items.includes(:item)
   end
- def create
+  
+  def create
     existing_cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
-  if existing_cart_item
-    # 存在した場合
-    # 2. カート内の個数をフォームから送られた個数分追加する
-    existing_cart_item.amount += params[:cart_item][:amount].to_i
-    if existing_cart_item.save
-      flash[:notice]="カートに商品が追加されました"
-      redirect_to cart_items_path
+    if existing_cart_item
+      # 存在した場合
+      # 2. カート内の個数をフォームから送られた個数分追加する
+      existing_cart_item.amount += params[:cart_item][:amount].to_i
+      if existing_cart_item.save
+        flash[:notice]="カートに商品が追加されました"
+        redirect_to cart_items_path
+      else
+        redirect_to item_path(params[:cart_item][:item_id])
+      end
     else
-      redirect_to item_path(params[:cart_item][:item_id])
-    end
-  else
-    # 存在しなかった場合
-    # カートモデルにレコードを新規作成する
-    @cart_item = current_customer.cart_items.new(cart_item_params)
-    if @cart_item.save
-      flash[:notice]="カートに商品を追加しました"
-      redirect_to cart_items_path
-    else
-      redirect_to item_path(params[:cart_item][:item_id])
+      # 存在しなかった場合
+      # カートモデルにレコードを新規作成する
+      @cart_item = current_customer.cart_items.new(cart_item_params)
+      if @cart_item.save
+        flash[:notice]="カートに商品を追加しました"
+        redirect_to cart_items_path
+      else
+        redirect_to item_path(params[:cart_item][:item_id])
+      end
     end
   end
- end
     
   def update
     @cart_item = current_customer.cart_items.find(params[:id])
@@ -58,6 +59,5 @@ class Public::CartItemsController < ApplicationController
 
   def cart_item_params
     params.require(:cart_item).permit(:item_id,:amount)
-  end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,9 @@
 Admin.create!(
-   email: 'admin@admin',
-   password: 'testtest'
+   email: "admin@admin",
+   password: "testtest"
 )
+
+Admin.find_or_create_by(id: 1) do |admin|
+  admin.email = "admin@admin"
+  admin.encrypted_password = "testtest"
+end

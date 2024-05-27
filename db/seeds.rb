46a3c6973fbd61bb5db9ba0b27e49b448d41a13f
Area6080 <area6080@gmail.com>
2024-05-27 06:51:02 +0000

    [Fix]search

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,16 @@
 Admin.create!(
-   email: "admin@admin",
-   password: "testtest"
+  [
+    {
+      email: "admin@admin",
+      password: "tttttt"
+    }
+  ]
 )
-
-Admin.find_or_create_by(id: 1) do |admin|
-  admin.email = "admin@admin"
-  admin.encrypted_password = "testtest"
-end
+# Admin.find_or_create_by(id: 1) do |admin|
+#   admin.email = "admin@admin"
+#   admin.encrypted_password = "testtest"
+# end
+# Admin.create!(
+#   email: "admin@admin",
+#   password: "tttttt"
+#   )
